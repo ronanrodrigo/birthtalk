952fe6864de0878f5a4d0fd6fec61fbf34b8d9ee
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-01-19 13:47:27 -0200

    Create user protocol and user entity

---
+++ Birthtalk/Authentications/Entities/User.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+protocol User: Equatable {
+    var identifier: String? { get }
+    var name: String { get }
+    var email: String { get }
+    var password: String { get }
+    var birthdate: Date { get }
+}
+
+struct UserEntity: User {
+    let identifier: String?
+    let name: String
+    let email: String
+    let password: String
+    let birthdate: Date
+}
+
+extension UserEntity: Equatable {
+    static func == (lhs: UserEntity, rhs: UserEntity) -> Bool {
+        return lhs.identifier == rhs.identifier &&
+            lhs.email == rhs.email
+    }
+}
