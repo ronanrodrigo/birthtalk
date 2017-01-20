4513cdeec85837ec2b6860d3e8a695d2f6a7a79e
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-01-20 16:37:49 -0200

    Create firebase gateway

--- Birthtalk/Authentications/Gateways/AuthenticationGateway.swift
+++ Birthtalk/Authentications/Gateways/AuthenticationGateway.swift
@@ -4,5 +4,5 @@ typealias RegisterResult = Result<UserEntity, RequestError>
 
 protocol AuthenticationGateway {
     func register(name: String, email: String, password: String, birthdate: Date,
-                  completion: ((RegisterResult) -> Void))
+                  completion: @escaping ((RegisterResult) -> Void))
 }
