4cc5d7655fecf89d1e1394164e28d9c7c326b26b
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-03-02 23:00:52 -0300

    Update validation steps

--- Birthtalk/Authentications/Usecases/RegisterUserUsecase.swift
+++ Birthtalk/Authentications/Usecases/RegisterUserUsecase.swift
@@ -11,7 +11,7 @@ struct RegisterUserUsecase {
     }
 
     func register(name: String, email: String, password: String, birthdate: Date) {
-        guard isValidInputs(name: name, email: email, password: password, birthdate: birthdate) else { return }
+        guard isValid(name: name, email: email, password: password) else { return }
         gateway.register(name: name, email: email, password: password, birthdate: birthdate, completion: presentResult)
     }
 
@@ -22,19 +22,21 @@ struct RegisterUserUsecase {
         }
     }
 
-    private func isValidInputs(name: String, email: String, password: String, birthdate: Date) -> Bool {
+    private func isValid(name: String, email: String, password: String) -> Bool {
         var errors = [AuthenticationError]()
         if name.isEmpty { errors.append(.invalidName) }
         if password.characters.count < 5 { errors.append(.invalidPassword) }
-        if email.isEmpty { errors.append(.invalidEmail) }
-        if !isValidEmail(email: email) { errors.append(.invalidEmail) }
+        if !isValid(email: email) { errors.append(.invalidEmail) }
 
-        if errors.count == 0 { return true }
         errors.forEach(presenter.failure)
-        return false
+
+        let isWhithoutErrors = errors.count == 0
+        return isWhithoutErrors
     }
 
-    private func isValidEmail(email: String) -> Bool {
+    private func isValid(email: String) -> Bool {
+        if email.isEmpty { return false }
+
         let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
 
