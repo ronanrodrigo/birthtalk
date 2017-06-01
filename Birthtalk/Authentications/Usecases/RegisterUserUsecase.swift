2ccb211b33ec22020e6f2de52faa7b8df9484214
Ronan Rodrigo Nunes <ronan.nunes@me.com>
2017-06-01 18:34:16 -0300

    Update regiser use case signature

--- Birthtalk/Authentications/Usecases/RegisterUserUsecase.swift
+++ Birthtalk/Authentications/Usecases/RegisterUserUsecase.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+typealias RegisterUserUsecaseParams = (name: String, email: String, password: String, birthdate: Date)
+
 struct RegisterUserUsecase {
 
     private let gateway: AuthenticationGateway
@@ -10,18 +12,20 @@ struct RegisterUserUsecase {
         self.presenter = presenter
     }
 
-    func register(name: String, email: String, password: String, birthdate: Date) {
+    func register(registerUserParams: RegisterUserUsecaseParams) {
         let invalidInputs: [AuthenticationErrorValidator] = [
-            ValidateEmailEntity(email: email),
-            ValidateNameEntity(name: name),
-            ValidatePasswordEntity(password: password)]
+            ValidateEmailEntity(email: registerUserParams.email),
+            ValidateNameEntity(name: registerUserParams.name),
+            ValidatePasswordEntity(password: registerUserParams.password)]
             .filter { !$0.isValid() }
 
         invalidInputs.forEach { presenter.failure(error: $0.error) }
 
         let isWithoutErrors = invalidInputs.count == 0
         if isWithoutErrors {
-            gateway.register(name: name, email: email, password: password, birthdate: birthdate, completion: presentResult)
+            gateway.register(name: registerUserParams.name, email: registerUserParams.email,
+                             password: registerUserParams.password, birthdate: registerUserParams.birthdate,
+                             completion: presentResult)
         }
     }
 
